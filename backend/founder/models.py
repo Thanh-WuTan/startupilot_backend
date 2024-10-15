5db422fb1d4866186576ce20d3bec3f4e6f0f14e
unknown <wt.vathanh@gmail.com>
2024-10-15 18:10:36 +0700

    update docker-compose.yml to auto make migrate

--- backend/founder/models.py
+++ backend/founder/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 class Founder(models.Model):
     name = models.CharField(max_length=255)
-    email = models.EmailField() 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
