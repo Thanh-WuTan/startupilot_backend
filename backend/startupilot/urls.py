3cc336ce2d8361560861c98222ee91f1f9f71dc7
Ai Thanh Vu <126098991+Thanh-WuTan@users.noreply.github.com>
2024-10-24 22:38:32 +0700

    update auth url

--- backend/startupilot/urls.py
+++ backend/startupilot/urls.py
@@ -5,5 +5,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls), 
-    path('api/', include('useraccount.urls'))
+    path('api/auth/', include('useraccount.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
