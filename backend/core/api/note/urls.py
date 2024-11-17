8654f9e3bd43de52d01b61d36a9310a905a7384f
unknown <wt.vathanh@gmail.com>
2024-11-17 22:14:38 +0700

    feat note moel and its apis

---
+++ backend/core/api/note/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .api import NoteDetailView, NoteCreateView
+
+urlpatterns = [
+    path('<uuid:pk>', NoteDetailView.as_view(), name='note-detail'),
+    path('create/', NoteCreateView.as_view(), name='note-create'),
+]
